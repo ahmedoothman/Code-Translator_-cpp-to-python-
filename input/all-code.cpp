96cb9e276524f7a143af25e4862b417baa079442
Ahmed Othman <ahmed.othman.ghazzawi@gmail.com>
2023-05-16 20:58:42 +0300

    upload

---
+++ input/all-code.cpp
@@ -0,0 +1,41 @@
+int main() {
+    
+ 	if (x < 8 || x> 4 
+        || true  || false  &&
+        ziad > 4 && ziad  < 4 
+        ){
+        
+    }else if (true && false){
+        for (int i =0 ; i <8; i ++){
+            while(x < 4){
+                int x = 44 ;
+                x = 8;
+            }
+            y = 4;
+        }
+    }else if (x >= 4 && x <= 4 && x!= 4){
+        do{
+            j = 8;
+            if (x<4){
+                int j = 4 ;
+            }
+            b= 4;
+            
+        }while(z < 8);
+    }else{
+        switch(jj){
+            case 0 :
+                int q = 4 ;
+                break ;
+            case 1 : 
+                int j = 1;
+              break;
+            default:
+                int y=4;
+             break ;
+        }
+    }
+    
+    
+    return 8 ;
+}
